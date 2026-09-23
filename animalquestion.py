d078d6a368648cf944a80598a3dab7f20e210667
preciousoyesanya11 <833422@mystma.org>
2026-09-23 13:32:39 -0500

    Add files via upload

---
+++ animalquestion.py
@@ -0,0 +1,10 @@
+fav_animal = input("what is your fav animal")
+Why_do_you_like_this_animal = input("why do you like this animal")
+where_do_they_live = input("where do they live")
+what_is_a_fun_fact = input("whats a fun fact about them")
+what_is_your_fav_breed_of_these = input("if there are different breeds, what is your fav one")
+
+print("----------------------------")
+
+summary = input("your favorite animal is " + fav_animal + " because " + Why_do_you_like_this_animal + "." + " They live in " + where_do_they_live + " and your favorite breed of them is " + what_is_your_fav_breed_of_these + "." + " A fun fact about them is " + what_is_a_fun_fact  + ".")
+
